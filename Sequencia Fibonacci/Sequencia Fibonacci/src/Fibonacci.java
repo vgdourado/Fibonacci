40b7c91f20fa3d96a610cc000928bfcfdccbc7ed
Vitória Gabriele <vitoriagdouradosouza008@gmail.com>
2026-09-25 11:01:57 -0400

    Add files via upload

---
+++ Sequencia Fibonacci/Sequencia Fibonacci/src/Fibonacci.java
@@ -0,0 +1,19 @@
+public class Fibonacci {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int qtd = 10;
+
+        for(int i =0; i < qtd; i++) {
+            System.out.print(fibonacci(i) + ", ");
+        }
+    }
+
+}
